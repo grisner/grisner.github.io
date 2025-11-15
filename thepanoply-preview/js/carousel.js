ca35d8a4ec279ee9c12dc7e428295dd255d44c84
Mikael Nilsson <micek@hotmail.com>
2025-11-15 18:53:43 +0100

    deploy preview thepanoply.se

--- thepanoply-preview/js/carousel.js
+++ thepanoply-preview/js/carousel.js
@@ -6,12 +6,12 @@ const imageNames = [
   "nilssons",
 ];
 
-const large = imageNames.map((img) => `images/carousel/${img}.webp`);
+const large = imageNames.map((img) => `images/carousel/${img}.thin.webp`);
 const small = imageNames.map((img) => `images/carousel/${img}.small.webp`);
 
 // Creating image elements
 const slideContainer = document.getElementById("slide_container");
-const navDots = document.getElementById("carousel_navdots");
+const navDots = document.getElementById("hem");
 const images = screen.width > 1000 ? large : small;
 
 images.forEach((url, id) => {
