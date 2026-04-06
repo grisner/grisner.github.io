dcae2d41274c139d35a4e2a6aeee61e984ade343
Mikael Nilsson <micek@hotmail.com>
2026-04-06 17:17:03 +0200

    deploy preview thepanoply.se

--- thepanoply-preview/js/form.js
+++ thepanoply-preview/js/form.js
@@ -1,23 +1,31 @@
 const baseUrl="https://docs.google.com/forms/d/e";
-const formId="1FAIpQLScTm5KoP2kXT1wh5mtM3DI4S84NzGfCh_NkO69MKzqWlwkMUg";
 
+/*## -- Prodform -- ##*/
+const _prms = {
+	id: "1FAIpQLSdwnB0_50U8xu14WWsWq9XKouXCk7J3jojb8-8JOxBzujZjpg",
+	mail: "entry.1085999444",
+	telno: "entry.1146311830",
+	place: "entry.749042839",
+	year: "entry.1762746159_year",
+	month: "entry.1762746159_month",
+	day: "entry.1762746159_day",
+}
 
 /*## -- Exempelform -- ##*/
 
-const mailPrm="entry.1349343839";
-const telnoPrm="entry.1662488425";
-const placePrm="entry.570129751";
-const yearPrm="entry.1026295849_year";
-const monthPrm="entry.1026295849_month";
-const dayPrm="entry.1026295849_day";
-
-console.log('6');
-
+const prms = {
+	id: "1FAIpQLScTm5KoP2kXT1wh5mtM3DI4S84NzGfCh_NkO69MKzqWlwkMUg",
+	mail: "entry.1349343839",
+	telno: "entry.1662488425",
+	place: "entry.570129751",
+	year: "entry.1026295849_year",
+	month: "entry.1026295849_month",
+	day: "entry.1026295849_day",
+}
 
 const post = (event) => {
 	console.log('posting data');
 	
-
 	// Collection
 	const mailAns=event.target.mail.value;
 	const telnoAns=event.target.telno.value;
@@ -29,12 +37,12 @@ const post = (event) => {
 	const dayAns=date.getDate();
 
 	// Composition
-	const mail=`${mailPrm}=${mailAns}`;
-	const telno=`${telnoPrm}=${telnoAns}`;
-	const place=`${placePrm}=${placeAns}`;
-	const year=`${yearPrm}=${yearAns}`;
-	const month=`${monthPrm}=${monthAns}`;
-	const day=`${dayPrm}=${dayAns}`;
+	const mail=`${prms.mail}=${mailAns}`;
+	const telno=`${prms.telno}=${telnoAns}`;
+	const place=`${prms.place}=${placeAns}`;
+	const year=`${prms.year}=${yearAns}`;
+	const month=`${prms.month}=${monthAns}`;
+	const day=`${prms.day}=${dayAns}`;
 
 	console.log({
 		mail,
@@ -55,16 +63,24 @@ const post = (event) => {
 		}
 	};
 
-	fetch(`${baseUrl}/${formId}/formResponse?${mail}&${telno}&${place}&${year}&${month}&${day}`, options);
+	fetch(`${baseUrl}/${prms.id}/formResponse?${mail}&${telno}&${place}&${year}&${month}&${day}`, options);
 
 	const okBtn = document.getElementById("submit");
 	okBtn.value = "Förfrågan skickad";
-	okBtn.classList += " disabled";
+	okBtn.classList = "btn disabled";
 	okBtn.disabled = true;
+	
 	
 
 	const toast = document.getElementById("sentStatus");
 	toast.style.visibility = "Visible";
 
 	return false;
+}
+
+const reEnableForm = () => {
+	console.log('reenabling form');
+	const okBtn = document.getElementById("submit");
+	okBtn.classList = "btn enabled";
+	okBtn.disabled = false;
 }
